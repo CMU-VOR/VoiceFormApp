d8f2189a9920a38ca2af644be3486ddaa4ea7b40
Xiaoxu Lu/Lv <xiaoxul@andrew.cmu.edu>
2015-11-28 22:57:54 -0500

    finish form 1 design and implementation

--- workOrderForm/Form1.cs
+++ workOrderForm/Form1.cs
@@ -21,13 +21,18 @@ namespace workOrderForm
             
         }
         // The handler to change the Text in the comboBox 
-        void ser_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        void name_ser_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             Console.WriteLine("Speech recognized:  " + e.Result.Text);
             MessageBox.Show("Speech recognized: " + e.Result.Text);
             NameBox.Text = e.Result.Text.Split().Last();
         }
-
+        void plane_ser_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Speech recognized:  " + e.Result.Text);
+            MessageBox.Show("Speech recognized: " + e.Result.Text);
+            PlaneBox.Text = e.Result.Text.Split().Last();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             
@@ -58,25 +63,42 @@ namespace workOrderForm
             wf.FormClosed += (s, args) => this.Close();
             wf.Show();
         }
-        
-        private void RecordButton2_Click(object sender, EventArgs e)
+        private Grammar CreateLogInGrammar()
         {
+            Choices nameChoices = new Choices(new string[] { "Ravi", "Ariel", "Bhiksha" });
+            GrammarBuilder nameElement = new GrammarBuilder(nameChoices);
+            Choices planeChoices = new Choices(new string[] { "787", "777", "747" });
+            GrammarBuilder planeElement = new GrammarBuilder(planeChoices);
+            // Create grammar builders for the two versions of the phrase.
+            GrammarBuilder namePhrase = new GrammarBuilder("The customer name is");
+            namePhrase.Append(nameElement);
+            GrammarBuilder planePhrase = new GrammarBuilder("The plane model is");
+            planePhrase.Append(planeElement);
 
+            // Create a Choices for the two alternative phrases, convert the Choices
+            // to a GrammarBuilder, and construct the grammar from the result.
+            Choices bothChoices = new Choices(new GrammarBuilder[] { namePhrase, planePhrase });
+            Grammar grammar = new Grammar((GrammarBuilder)bothChoices);
+            grammar.Name = "loginGrammar";
+            return grammar;
         }
         SpeechRecognizer recognizer;
-        GrammarBuilder gb;
-        Choices names;
         Grammar g;
+        private void RecordButton2_Click(object sender, EventArgs e)
+        {
+            recognizer = new SpeechRecognizer();
+            g = CreateLogInGrammar();
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(plane_ser_SpeechRecognized);
+            recognizer.LoadGrammar(g);
+        }
+
         private void RecordButton1_Click(object sender, EventArgs e)
         {
             recognizer = new SpeechRecognizer();
-            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(ser_SpeechRecognized);
-            gb = new GrammarBuilder("The customer name is");
-            names = new Choices();
-            names.Add(new String[] { "Ravi", "Michael", "Ariel", "Sansa", "Bhiksha" });
-            gb.Append(names);
-            g = new Grammar(gb);
+            g = CreateLogInGrammar();
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(name_ser_SpeechRecognized);
             recognizer.LoadGrammar(g);
+
         }
 
         private void NameBox_TextChanged(object sender, EventArgs e)
